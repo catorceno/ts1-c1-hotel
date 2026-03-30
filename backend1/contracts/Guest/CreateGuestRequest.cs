c69004ff4192988c75be3b2d728cf57618453df9
catorceno <camilacatorceno005@gmail.com>
2026-03-30 18:40:34 -0400

    cleaning

--- backend1/contracts/Guest/CreateGuestRequest.cs
+++
@@ -1,8 +0,0 @@
-
-public class CreateGuestRequest
-{
-    public string Name {get; set;}
-    public string Ci { get; set; }
-    public string Email { get; set; }
-    public string Phone { get; set; }
-}
